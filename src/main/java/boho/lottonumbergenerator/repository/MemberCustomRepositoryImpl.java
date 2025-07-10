2db19e6f885a14ae3d343e8b68a0d35fd9291cc0
Jprotection <bigho98@gmail.com>
2025-07-10 20:30:52 +0900

    inner join을 left join으로 변경

--- src/main/java/boho/lottonumbergenerator/repository/MemberCustomRepositoryImpl.java
+++ src/main/java/boho/lottonumbergenerator/repository/MemberCustomRepositoryImpl.java
@@ -22,8 +22,9 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
 	public MemberInfoResponse findMemberInfo(String username) {
 		Map<String, MemberInfoResponse> resultMap = queryFactory
 			.from(member)
-			.innerJoin(memberTitle).on(member.eq(memberTitle.member))
-			.innerJoin(title).on(memberTitle.title.eq(title))
+			// memberTitle이 없는 member는 INNER JOIN에 의해 조회되지 않아서 resultMap.get(username) 결과가 null이 됨
+			.leftJoin(memberTitle).on(member.eq(memberTitle.member))
+			.leftJoin(title).on(memberTitle.title.eq(title))
 			.where(member.username.eq(username))
 			.transform(groupBy(member.username).as(
 				Projections.constructor(MemberInfoResponse.class,
@@ -32,6 +33,7 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
 					member.authority,
 					member.createDate,
 					member.lastLoginDate,
+					// title이 없어도 빈 리스트로 들어감
 					list(title.name)
 				)
 			));
